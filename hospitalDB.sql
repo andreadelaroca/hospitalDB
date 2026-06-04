c137e1a07c97be995a2de8dad2dc2c11af4e5c6d
andreadelaroca <asodelaroca@uamv.edu.ni>
2026-06-04 07:57:23 -0600

    feat(): salario válido

--- hospitalDB.sql
+++ hospitalDB.sql
@@ -36,7 +36,7 @@ CREATE TABLE Gestiones.Pacientes (
 	idPaciente INT IDENTITY(1,1) CONSTRAINT PK_idPaciente PRIMARY KEY --11. PK Pacientes
 	, nombre NVARCHAR(60) NOT NULL --13. nombre NOT NULL 
 	, correo NVARCHAR(100) CONSTRAINT UQ_correo_pac UNIQUE --15. correo UNIQUE
-	, fechanac DATE  
+	, fechanac DATE CONSTRAINT CK_edad_val_pac CHECK(fechanac >= GETDATE()) --17. CHECK para edad mayor o igual a 0.
 )
 GO
 
@@ -45,6 +45,7 @@ CREATE TABLE Empleados.Medicos (
 	idMedico INT IDENTITY(1,1) CONSTRAINT PK_idMedico PRIMARY KEY --12. PK Medicos
 	, nombre NVARCHAR(60) NOT NULL --14. nombre NOT NULL 
 	, correo NVARCHAR(100) CONSTRAINT UQ_correo_med UNIQUE --16. correo UNIQUE
+	, fechanac DATE CONSTRAINT CK_edad_val_med CHECK(fechanac >= GETDATE()) --17. CHECK para edad mayor o igual a 0.
 )
 GO
 
