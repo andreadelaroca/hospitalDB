f4cb0de9fa0e022ae14c880fb35b1e332807b331
andreadelaroca <asodelaroca@uamv.edu.ni>
2026-06-04 07:48:33 -0600

    feat(): PK de Pacientes

--- hospitalDB.sql
+++ hospitalDB.sql
@@ -31,25 +31,34 @@ GO
 CREATE SCHEMA Hospital
 GO
 
---4.2 Creación de tablas
-CREATE TABLE Pacientes.Pacientes
+--4. Creación de tabla Pacientes
+CREATE TABLE Pacientes.Pacientes (
+	idPaciente INT IDENTITY(1,1) CONSTRAINT PK_idPaciente PRIMARY KEY --11. PK Pacientes
+)
 GO
 
+--5. Creación de tabla Medicos
 CREATE TABLE Medicos.Medicos
+	idMedico INT IDENTITY(1,1) CONSTRAINT PK_idMedico PRIMARY KEY --12. PK Medicos
 GO
 
+--6. Creación de tabla Especialidades
 CREATE TABLE Medicos.Especialidades
 GO
 
+--7. Creación de tabla Citas
 CREATE TABLE Pacientes.Citas
 GO
 
+--8. Creación de tabla Habitaciones
 CREATE TABLE Hospital.Habitaciones
 GO
 
+--9. Creación de tabla Medicamentos
 CREATE TABLE Hospital.Medicamentos
 GO
 
+--10. Creación de tabla Tratamientos
 CREATE TABLE Hospital.Tratamientos
 GO
 
