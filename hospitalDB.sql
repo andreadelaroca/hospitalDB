9957b0220ae74f1eb13e0e33a03d396e47536fad
andreadelaroca <asodelaroca@uamv.edu.ni>
2026-06-04 07:34:52 -0600

    feat(): Creación de la BD

--- hospitalDB.sql
+++ hospitalDB.sql
@@ -1,1 +1,18 @@
---Inicio de repositorio
+--MÓDULO 1: CREACIÓN DE LA BASE DE DATOS 
+USE master
+GO
+
+IF EXISTS(SELECT * FROM sys.databases WHERE NAME = 'HospitalDB')
+	BEGIN
+		ALTER DATABASE HospitalDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+		DROP DATABASE HospitalDB
+	END
+GO
+
+--1. Crear una base de datos llamada HospitalDB
+CREATE DATABASE HospitalDB
+GO
+
+--2. Mostrar todas las bases de datos existentes.
+SELECT * FROM sys.databases
+GO
